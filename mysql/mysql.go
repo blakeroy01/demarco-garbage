d0863330e7aefb1ff0681c71f9da3558b45fde61
Blake Roy <blake@Blakes-MacBook-Pro.local>
2021-11-26 16:58:35 -0500

    Cleanup and helper functions

--- mysql/mysql.go
+++ mysql/mysql.go
@@ -9,6 +9,7 @@ type MySQLDatabase struct {
 	connection *sql.DB
 }
 
+// Connect is a helper function to connect to a MySQL instance
 func Connect() (*MySQLDatabase, error) {
 	db, err := sql.Open("mysql", "root:astronomicstudios@tcp(127.0.0.1:3306)/disposal_schema")
 	if err != nil {
