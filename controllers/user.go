c1eacd3617399ee371ae96de62dcfc465d38a192
JebadiahParillo <JEBADIAHPARILLO@GMAIL.COM>
2021-11-28 18:50:32 -0500

    Skeleton-out-the-user-controller

---
+++ controllers/user.go
@@ -0,0 +1,25 @@
+package controllers
+
+// Createuser will receive the users information as a request
+//  and then store the Users information in the user model in the MySQL Database Server
+func createUser() {
+
+}
+
+// Read will receive the user model contents of a request
+// then retreive the specified user model in the MySQL Database Server
+func readUser() {
+
+}
+
+// Update will receive the body contents of a request
+// then update the specified item in the MySQL Database Server
+func updateUser() {
+
+}
+
+// Delete will receive the body contents of a request
+// then delete the specified item in the MySQL Database Server
+func deleteUser() {
+
+}
