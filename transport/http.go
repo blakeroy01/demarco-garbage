8415a7e621af1c74cb1362a94c85524105312c02
JebadiahParillo <JEBADIAHPARILLO@GMAIL.COM>
2021-12-01 17:59:30 -0500

    Skeleton-out-LoginUser-RequestHandler

--- transport/http.go
+++ transport/http.go
@@ -43,6 +43,8 @@ func (handler *APIHandler) LoginUser() http.Handler {
 	return http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
 		if req.Method == http.MethodGet {
 			res.Write([]byte("GET"))
+		} else if req.Method == http.MethodPost {
+			res.Write([]byte("POST"))
 		} else {
 			http.Error(res, "Unsupported HTTP Method", http.StatusBadRequest)
 		}
@@ -53,6 +55,7 @@ func (handler *APIHandler) CreateMultiplexer() *http.ServeMux {
 	multiplexer := http.NewServeMux()
 	multiplexer.Handle("/", handler.HomePage())
 	multiplexer.Handle("/user/register", handler.RegisterUser())
+	multiplexer.Handle("/user/login", handler.LoginUser())
 	multiplexer.Handle("/payments", handler.PaymentInfo())
 
 	return multiplexer
