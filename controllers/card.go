454bb05506b8fbfffc263dedd0c06a0a0d025c23
Blake Roy <blakeroy01@gmail.com>
2021-12-03 14:01:50 -0500

    Fixed SQL Syntax

--- controllers/card.go
+++ controllers/card.go
@@ -16,9 +16,19 @@ func CreateCard(requestBody *io.ReadCloser, db *mysql.MySQLDatabase) {
 	card := models.Card{}
 	json.NewDecoder(*requestBody).Decode(&card)
 
-	sql := fmt.Sprintf("INSERT INTO card(card_number, name_on_card, cvv, expiration_date, billing_address, billing_zip, billing_state) VALUES (%s, %s, %s, %s, %s, %s, %s)", card.CardNumber, card.NameOnCard,
+	// Remove this upon deployment 
+	card.CardNumber = "'4444444444444444'"
+	card.NameOnCard = "'Jeb & Blakes Credit Card'"
+	card.CVV = "'679'"
+	card.ExpirationDate = "'02/25'"
+	card.BillingAddress = "'123 Fetty Wap Drive'"
+	card.BillingZip = "'33409'"
+	card.BillingState = "'NY'"
+
+	sql := fmt.Sprintf("INSERT INTO card(card_number, name_on_card, cvv, expiration_date, billing_address, billing_zip, billing_state) VALUES (%s, %s, %s, %s, %s, %s, %s);", card.CardNumber, card.NameOnCard,
 		card.CVV, card.ExpirationDate, card.BillingAddress, card.BillingZip,
 		card.BillingState)
+
 	_, err := db.Connection.Exec(sql)
 	if err != nil {
 		log.Fatal(err)
